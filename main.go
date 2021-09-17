4503b3bd36342389bd65b95f1a5b3c8562236469
theterminalguy <damiansimonpeter@gmail.com>
2021-09-17 17:40:49 -0400

    [INT-66] Add Echo as dependency

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/10hourlabs/jobsapi/internal/job"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	j := &job.Job{}
-	fmt.Println(j.Hello())
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, j.Hello())
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
