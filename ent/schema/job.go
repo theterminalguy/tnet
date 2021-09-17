439fda33861183c10ace3d2e8096d8a5c9f48ab2
theterminalguy <damiansimonpeter@gmail.com>
2021-09-17 19:05:14 -0400

    [INT-73] Add fields

--- ent/schema/job.go
+++ ent/schema/job.go
@@ -13,45 +13,6 @@ type Job struct {
 	ent.Schema
 }
 
-/*
-    id: 1,
-    hiring: false,
-    categoryId: 2,
-    title: 'Senior Full Stack Developer',
-    slug: 'senior-full-stack-developer',
-    location: 'Remote, Earth',
-    summary:
-      'Are you looking to make an impact with a dynamic team that is 100% remote with challenging opportunities? Then consider joining our team',
-    type: 'Full-Time',
-    category: 'Engineering',
-    thumbnail: '/assets/images/cardOne.png',
-    weHave: [
-      'Employer-paid health care',
-      'Perks for electricity, internet and more',
-      'Casual and diverse workplace',
-      'Paid vacation and team events',
-    ],
-    requirements: [
-      'Energized to join a startup',
-      'Excited to mentor more junior developers',
-      'Good at problem selection, problem',
-      'Solving, and course correcting',
-      'Focused on best practices Highly pragmatic and collaborative',
-    ],
-    youHave: [
-      'At least 4+ years of professional work experience as a Software Developer',
-      'Proficient in Python and PHP',
-      'Understanding of OOP principles and practices',
-      'Experience working with relational databases MySQL and PostgreSQL',
-      'You enjoy working with legacy code. Perhaps you are that one guy who has read Micheal Feathers',
-      'Version control and Git workflow',
-      'You enjoy Front-End development',
-      'Understand Containers, Infrastructure as Code and have worked with various cloud providers',
-      'You enjoy writing tests',
-    ],
-  },
-*/
-
 // Fields of the Job.
 func (Job) Fields() []ent.Field {
 	return []ent.Field{
