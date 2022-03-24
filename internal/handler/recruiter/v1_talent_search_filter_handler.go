7a04cd99ae1b79034a14c63f59ad05523dc32f4b
Askari <blgntobi@gmail.com>
2022-03-24 13:16:32 +0100

    Intr 429 get talent by UUID should return country object (#105)
    * wip
    
    * INTR-429-get-talent-by-uuid-should-return-country-object
    
    Co-authored-by: Oloruntobi1 <blgntobi@gmail>

--- internal/handler/recruiter/v1_talent_search_filter_handler.go
+++ internal/handler/recruiter/v1_talent_search_filter_handler.go
@@ -57,7 +57,7 @@ func (v *V1TalentSearchFilterHandler) ReadByID(c echo.Context) error {
 	if err != nil {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
-	record, err := v.TalentRepo.GetByUUID(id)
+	record, err := v.TalentRepo.GetTalentByUUID(id)
 	if err != nil {
 		return c.String(http.StatusNotFound, err.Error())
 	}
