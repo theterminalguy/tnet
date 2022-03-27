71aafa31df8488a46299c8bebdac80d49f136692
theterminalguy <damiansimonpeter@gmail.com>
2022-03-27 10:53:54 -0400

    [INTR-338] Redirect to slack

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"text/template"
 
 	"github.com/10hourlabs/tentn/internal/router"
+	"github.com/labstack/echo/v4"
 )
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	e := router.DefineRoutes()
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = renderer
+
 	httpPort := fmt.Sprintf(":%v", os.Getenv("PORT"))
 	if os.Getenv("ENV") == "dev" {
 		e.Logger.Fatal(e.StartTLS(httpPort, "cert.pem", "cert-key.pem"))
--- internal/handler/index_handler.go
+++ internal/handler/index_handler.go
@@ -7,33 +7,5 @@ import (
 )
 
 func IndexHandler(c echo.Context) error {
-	html := `<!DOCTYPE html>
-<html>
-<head>
-<title>Tentn</title>
-</head>
-<body>
-<h1>Welcome to the future of Recruiting</h1> 
-<i>Talent Network API version 0.0.1</i>
-
-<h2>Features</h2>
-<ul>
-<li>AI Powered Job Recruiting and Matching Engine</li>
-<li>Automate your JOB posting and searching process</li>
-<li>Use Natural Language to interface with our AI powered job search engine</li>
-</ul>
-
-
-<p>
-	<a href="/recruiter/auth">Add to Slack </a>
-</p>
-
-<p>
-	<a href="/talent/auth">Talent Login</a>
-</p>
-
-</body>
-</html>`
-	// render html
-	return c.HTMLBlob(http.StatusOK, []byte(html))
+	return c.Render(http.StatusOK, "index.html", nil)
 }
