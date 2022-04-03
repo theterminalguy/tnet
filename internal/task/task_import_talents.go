c10bf63ab4b3adbb426a7eb2eb464ad4d4367059
theterminalguy <damiansimonpeter@gmail.com>
2022-04-03 15:57:36 -0400

    [INTR-103] Extract all data from payload

--- internal/task/task_import_talents.go
+++ internal/task/task_import_talents.go
@@ -24,26 +24,22 @@ func NewImportTalents() *ImportTalents {
 	}
 }
 
-func (t *ImportTalents) CreateFakeTalent() error {
-	return nil
-}
-
 type UpsertUserParams struct {
 	ID                    uuid.UUID
-	FirstName             string                      `json:"first_name"`
-	LastName              string                      `json:"last_name"`
-	Pronoun               string                      `json:"pronoun"`
-	PreferredJobTitle     string                      `json:"preferred_job_title"`
-	Email                 string                      `json:"email"`
-	Phone                 string                      `json:"phone"`
-	ProfessionalSummary   string                      `json:"professional_summary"`
-	City                  string                      `json:"city"`
-	CountryCode           string                      `json:"country_code"`
-	ProfessionalStartDate string                      `json:"professional_start_date"`
-	Educations            []repo.EducationParams      `json:"educations"`
-	WorkExperiences       []repo.WorkExperienceParams `json:"experiences"`
-	Skills                []repo.SkillParams          `json:"skills"`
-	Portfolios            []repo.PortfolioLinkParams  `json:"portfolios"`
+	FirstName             string                       `json:"first_name"`
+	LastName              string                       `json:"last_name"`
+	Pronoun               string                       `json:"pronoun"`
+	PreferredJobTitle     string                       `json:"preferred_job_title"`
+	Email                 string                       `json:"email"`
+	Phone                 string                       `json:"phone"`
+	ProfessionalSummary   string                       `json:"professional_summary"`
+	City                  string                       `json:"city"`
+	CountryCode           string                       `json:"country_code"`
+	ProfessionalStartDate string                       `json:"professional_start_date"`
+	Educations            []*repo.EducationParams      `json:"educations"`
+	WorkExperiences       []*repo.WorkExperienceParams `json:"experiences"`
+	Skills                []*repo.SkillParams          `json:"skills"`
+	Portfolios            []*repo.PortfolioLinkParams  `json:"portfolios"`
 }
 
 func (t *ImportTalents) Run(_ string) error {
@@ -53,6 +49,7 @@ func (t *ImportTalents) Run(_ string) error {
 	}
 	defer f.Close()
 
+	var tds []*TalentData
 	csvReader := csv.NewReader(f)
 	index := 0
 	for {
@@ -63,17 +60,27 @@ func (t *ImportTalents) Run(_ string) error {
 		if err != nil {
 			return err
 		}
-		payload := record[1]
-		bulkUpsertTalent([]byte(payload))
-		if index == 5 {
+		payload := []byte(record[1])
+		tds = append(tds, extractTalentData(payload))
+		if index == 20 {
 			break
 		}
 		index++
 	}
+	fmt.Println(tds)
 	return nil
 }
 
-func bulkUpsertTalent(record []byte) {
+type TalentData struct {
+	User            *repo.UserParams
+	Talent          *repo.TalentParams
+	Skills          []*repo.SkillParams
+	WorkExperiences []*repo.WorkExperienceParams
+	Educations      []*repo.EducationParams
+	Portfolios      []*repo.PortfolioLinkParams
+}
+
+func extractTalentData(record []byte) *TalentData {
 	var uup UpsertUserParams
 	err := json.Unmarshal(record, &uup)
 	if err != nil {
@@ -83,7 +90,7 @@ func bulkUpsertTalent(record []byte) {
 		ID:        uuid.New(),
 		FirstName: uup.FirstName,
 		LastName:  uup.LastName,
-		Email:    uup.Email,
+		Email:     uup.Email,
 		PhotoURL: fmt.Sprintf(
 			"https://ui-avatars.com/api/?name=%s+%s&background=random&size=64",
 			uup.FirstName,
@@ -101,7 +108,7 @@ func bulkUpsertTalent(record []byte) {
 	fmt.Println("\tEmail: ", user.Email)
 	fmt.Println("\tRole: ", user.Role)
 	fmt.Println("\tApproved: ", user.Approved)
-	fmt.Print("==========================\n\n")
+	fmt.Print("\t==========================\n\n")
 
 	talent := &repo.TalentParams{
 		ID:                    uuid.New(),
@@ -141,4 +148,72 @@ func bulkUpsertTalent(record []byte) {
 	fmt.Println("\t\tAvailable: ", talent.Available)
 	fmt.Println("\t\tProfessional Summary: ", talent.ProfessionalSummary)
 
+	fmt.Println("\tSkills: ")
+	for _, skill := range uup.Skills {
+		// update skill IDs
+		skill.ID = uuid.New()
+		skill.TalentID = talent.ID
+		skill.Preferred = true
+		fmt.Println("\t\tID: ", skill.ID)
+		fmt.Println("\t\tTalentID: ", skill.TalentID)
+		fmt.Println("\t\tYears Of Experience: ", skill.YearsOfExperience)
+		fmt.Println("\t\tPreferred: ", skill.Preferred)
+		fmt.Println("\t\tNote: ", skill.Note)
+		fmt.Println("\t\tName: ", skill.Name)
+		fmt.Print("\t\t==========================\n\n")
+	}
+
+	fmt.Println("\tWork Experiences: ")
+	for _, we := range uup.WorkExperiences {
+		// update work experience IDs
+		we.ID = uuid.New()
+		we.TalentID = talent.ID
+		fmt.Println("\t\tID: ", we.ID)
+		fmt.Println("\t\tTalentID: ", we.TalentID)
+		fmt.Println("\t\tCompany: ", we.CompanyName)
+		fmt.Println("\t\tJob Title: ", we.JobTitle)
+		fmt.Println("\t\tPrimary Technologies: ", we.PrimaryTechnologies)
+		fmt.Println("\t\tDescription: ", we.Description)
+		fmt.Println("\t\tStartDate: ", we.StartDate)
+		fmt.Println("\t\tEndDate: ", we.EndDate)
+		fmt.Print("\t\t==========================\n\n")
+	}
+
+	fmt.Println("\tEducations: ")
+	for _, ed := range uup.Educations {
+		// update education IDs
+		ed.ID = uuid.New()
+		ed.TalentID = talent.ID
+		fmt.Println("\t\tID: ", ed.ID)
+		fmt.Println("\t\tTalentID: ", ed.TalentID)
+		fmt.Println("\t\tInstitution Name: ", ed.InstitutionName)
+		fmt.Println("\t\tLocation: ", ed.Location)
+		fmt.Println("\t\tDegree: ", ed.Degree)
+		fmt.Println("\t\tProgram: ", ed.Program)
+		fmt.Println("\t\tOverview: ", ed.Overview)
+		fmt.Println("\t\tStartDate: ", ed.StartDate)
+		fmt.Println("\t\tEndDate: ", ed.EndDate)
+	}
+
+	fmt.Println("\tPortfolios: ")
+	for _, p := range uup.Portfolios {
+		// update portfolio IDs
+		p.ID = uuid.New()
+		p.TalentID = talent.ID
+		fmt.Println("\t\tID: ", p.ID)
+		fmt.Println("\t\tTalentID: ", p.TalentID)
+		fmt.Println("\t\tName: ", p.Name)
+		fmt.Println("\t\tURL: ", p.URL)
+	}
+
+	fmt.Println("")
+
+	return &TalentData{
+		User:            user,
+		Talent:          talent,
+		Skills:          uup.Skills,
+		Educations:      uup.Educations,
+		WorkExperiences: uup.WorkExperiences,
+		Portfolios:      uup.Portfolios,
+	}
 }
