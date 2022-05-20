1b75e6521fd5f5a638f5509043704d37c8db363d
theterminalguy <damiansimonpeter@gmail.com>
2022-05-20 06:38:12 -0400

    [AH-78] Allow users to turn off SSL in dev
    - Provide a way to start the server without SSL

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,10 @@ func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c 
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+func inDevMode() bool {
+	return strings.HasPrefix(os.Getenv("ENV"), "dev")
+}
+
 func main() {
 	e := router.DefineRoutes()
 	e.Static("/public/views/css", "public/views/css")
@@ -31,9 +35,11 @@ func main() {
 	e.Renderer = renderer
 
 	httpPort := fmt.Sprintf(":%v", os.Getenv("PORT"))
-	if strings.HasPrefix(os.Getenv("ENV"), "dev") {
+	if inDevMode() && os.Getenv("SSL") == "" {
+		fmt.Println("Running in development mode with SSL")
 		e.Logger.Fatal(e.StartTLS(httpPort, "cert.pem", "cert-key.pem"))
 	} else {
+		fmt.Println("Running in development mode without SSL")
 		// Cloud Run automatically enforces TLS
 		e.Logger.Fatal(e.Start(httpPort))
 	}
