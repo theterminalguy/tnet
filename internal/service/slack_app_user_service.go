102260bc0a4a9462b5a6c7f58e0accedb0d5327a
theterminalguy <damiansimonpeter@gmail.com>
2022-05-20 04:23:27 -0400

    [AH-78] Add slack package for golang

--- internal/service/slack_app_user_service.go
+++ internal/service/slack_app_user_service.go
@@ -1,7 +1,18 @@
 package service
 
+import (
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
 type SlackAppUserService struct{}
 
 func NewSlackAppUserService() *SlackAppUserService {
 	return &SlackAppUserService{}
+}
+
+func (s *SlackAppUserService) GetUserInfo(slackUserID string) (*slack.User, error) {
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	return api.GetUserInfo(slackUserID)
 }
