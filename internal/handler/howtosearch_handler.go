147f0d521fd430bc4fe16fbe0ff19276fc8969cd
Abiodun Azeez <iamhabbeboy@gmail.com>
2022-06-27 15:26:50 +0100

    Remove const variable

--- internal/handler/howtosearch_handler.go
+++ internal/handler/howtosearch_handler.go
@@ -2,8 +2,6 @@ package handler
 
 import "github.com/labstack/echo/v4"
 
-const URL = "https://help.alwayshiring.io/"
-
 func HowToSearchHandler(c echo.Context) error {
-	return c.Redirect(302, URL)
+	return c.Redirect(302, "https://help.alwayshiring.io/")
 }
